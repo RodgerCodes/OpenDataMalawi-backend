032c888a1f418aa6d27c1c315e636d26a7e67124
Rodger Kumwanje <51970312+RodgerCodes@users.noreply.github.com>
2023-07-05 18:51:58 +0200

    chore: added Home app

--- Data_Library/Paths/upload_paths.py
+++ Data_Library/Paths/upload_paths.py
@@ -4,7 +4,7 @@ import os
 def get_file_format_logo_path(instance, filename):
     return os.path.join(
         "fileFormat", "%s" % instance.name, filename
-    )
+     )
 
 
 def get_dataset_file_path(instance, filename):
--- Server/settings.py
+++ Server/settings.py
@@ -25,7 +25,8 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'User.apps.UserConfig',
-    'Data_Library.apps.DataLibraryConfig'
+    'Data_Library.apps.DataLibraryConfig',
+    'Home.apps.HomeConfig',
 ]
 
 MIDDLEWARE = [
@@ -45,7 +46,7 @@ ROOT_URLCONF = 'Server.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR, "templates")],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -63,27 +64,27 @@ WSGI_APPLICATION = 'Server.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
-# DATABASES = {
-# 'default': {
-# 'ENGINE': 'django.db.backends.postgresql',
-# 'NAME': os.environ.get('POSTGRES_NAME'),
-# 'USER': os.environ.get('POSTGRES_USER'),
-# 'PASSWORD': os.environ.get('POSTGRES_PASSWORD'),
-# 'HOST': 'db',
-# 'PORT': 5432,
-# }
-# }
-
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'opendata',
-        'USER': 'postgres',
-        'PASSWORD': 'adminuser',
-        'HOST': 'localhost',
-        'PORT': 5432,
-    }
+'default': {
+'ENGINE': 'django.db.backends.postgresql',
+'NAME': os.environ.get('POSTGRES_NAME'),
+'USER': os.environ.get('POSTGRES_USER'),
+'PASSWORD': os.environ.get('POSTGRES_PASSWORD'),
+'HOST': 'db',
+'PORT': 5432,
 }
+}
+
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.postgresql',
+#         'NAME': 'opendata',
+#         'USER': 'postgres',
+#         'PASSWORD': 'adminuser',
+#         'HOST': 'localhost',
+#         'PORT': 5432,
+#     }
+# }
 
 # DATABASES = {
 #     'default': {
