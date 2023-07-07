08fe1813f78ff34a120ce04f78c09b25461ba68d
Rodger Kumwanje <51970312+RodgerCodes@users.noreply.github.com>
2023-07-07 23:31:38 +0200

    chore: media + static route setup

--- Server/settings.py
+++ Server/settings.py
@@ -128,6 +128,12 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 
+STATICFILES_DIRS = (os.path.join(BASE_DIR, "static"),)
+
+MEDIA_URL = "/Uploads/"
+
+MEDIA_ROOT = os.path.join(BASE_DIR, "Uploads")
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
