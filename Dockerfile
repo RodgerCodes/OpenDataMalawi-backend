d2095ce485ae76cbb948242d97d6506024814339
Rodger Kumwanje <51970312+RodgerCodes@users.noreply.github.com>
2023-07-08 22:43:27 +0200

    feat: tailwindcss setup

--- Dockerfile
+++ Dockerfile
@@ -1,6 +1,14 @@
 FROM python:3.10
 ENV PYTHONBUFFERED=1
 WORKDIR /code
+
 COPY requirements.txt /code/
+COPY package.json /code/
+
 RUN pip install -r requirements.txt
+RUN curl -sL https://deb.nodesource.com/setup_14.x | bash -
+
+RUN apt-get install -y nodejs
+RUN npm install
+
 COPY . /code/
--- Server/settings.py
+++ Server/settings.py
@@ -27,6 +27,7 @@ INSTALLED_APPS = [
     'User.apps.UserConfig',
     'Data_Library.apps.DataLibraryConfig',
     'Home.apps.HomeConfig',
+    'compressor',
 ]
 
 MIDDLEWARE = [
@@ -42,6 +43,12 @@ MIDDLEWARE = [
 AUTH_USER_MODEL = "User.User"
 
 ROOT_URLCONF = 'Server.urls'
+
+COMPRESS_ROOT = BASE_DIR / 'static'
+
+COMPRESS_ENABLED = True
+
+STATICFILES_FINDERS = ('compressor.finders.CompressorFinder',)
 
 TEMPLATES = [
     {
@@ -64,27 +71,27 @@ WSGI_APPLICATION = 'Server.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
-DATABASES = {
-'default': {
-'ENGINE': 'django.db.backends.postgresql',
-'NAME': os.environ.get('POSTGRES_NAME'),
-'USER': os.environ.get('POSTGRES_USER'),
-'PASSWORD': os.environ.get('POSTGRES_PASSWORD'),
-'HOST': 'db',
-'PORT': 5432,
-}
-}
-
 # DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.postgresql',
-#         'NAME': 'opendata',
-#         'USER': 'postgres',
-#         'PASSWORD': 'adminuser',
-#         'HOST': 'localhost',
-#         'PORT': 5432,
-#     }
+# 'default': {
+# 'ENGINE': 'django.db.backends.postgresql',
+# 'NAME': os.environ.get('POSTGRES_NAME'),
+# 'USER': os.environ.get('POSTGRES_USER'),
+# 'PASSWORD': os.environ.get('POSTGRES_PASSWORD'),
+# 'HOST': 'db',
+# 'PORT': 5432,
 # }
+# }
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'opendata',
+        'USER': 'postgres',
+        'PASSWORD': 'adminuser',
+        'HOST': 'localhost',
+        'PORT': 5432,
+    }
+}
 
 # DATABASES = {
 #     'default': {
