c63775fd453a5be035cc2a5424cdc106bb296927
Rodger Kumwanje <51970312+RodgerCodes@users.noreply.github.com>
2023-07-09 11:17:26 +0200

    chore: sign in page

--- Server/urls.py
+++ Server/urls.py
@@ -5,5 +5,6 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include("Home.urls"))
+    path('', include("Home.urls")),
+    path('account/', include("User.urls"))
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- User/urls.py
+++ User/urls.py
@@ -1,5 +1,7 @@
 from django.urls import path
+from . import views
 
+app_name="user"
 urlpatterns = [
-
+   path('', views.SignIn.as_view(), name="get_sign_in")
 ]
